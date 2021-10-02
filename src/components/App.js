b54514416e722b85d1143d4e4ddc3225d8314286
Heneros <punk60826@gmail.com>
2021-10-02 23:20:22 +0300

    routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () =>{
+    return (
+        <div>
+        PageOne
+        <Link to="/pagetwo">Navigate to page two</Link>
+</div>
+    )
+}
+
+const PageTwo = () =>{
+    return (
+        <div>
+        PageOne
+        <Link to="/pageone">Navigate to page one</Link>
+</div>
+    )
+}
+
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+        <BrowserRouter>
+        <div>
+            <Route page="/" exact component={PageOne} />
+            <Route page="/pagetwo" component={PageTwo} />
+            </div>
+        </BrowserRouter>
+    </div>
+    );
 };
 
 export default App;
