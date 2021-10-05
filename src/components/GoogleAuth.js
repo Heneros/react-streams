733c7248ae14d80a9362aa14e440a2cca61b161f
Heneros <punk60826@gmail.com>
2021-10-06 00:45:42 +0300

    signOut and signIn

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component{
+  state = { isSignedIn: null};
+  
     componentDidMount(){
         window.gapi.load('client:auth2', () =>{
             window.gapi.client.init({
                  clientId: '',
                  scope: 'email'
+            }).then(() =>{
+              this.auth = window.gapi.auth2.getAuthInstance();
+              this.setState({isSignedIn: this.auth.isSignedIn.get()})
             });
         });
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return <div>You probably not signed in.</div>
+        }else if (this.state.isSignedIn){
+            return <div>You're signed in!</div>
+        }else{
+            return <div>You're not signed in!</div>
+        }
     }
     render(){
         return(
             <div>
-            Google Auth
+           {this.renderAuthButton()}
         </div>
         )
        
