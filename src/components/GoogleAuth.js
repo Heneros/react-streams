b326948a6c4f8674e5a7cb07635b1191cedbc825
Heneros <punk60826@gmail.com>
2021-10-08 01:28:02 +0300

    signIn and signOut btns

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -10,18 +10,42 @@ class GoogleAuth extends React.Component{
                  scope: 'email'
             }).then(() =>{
               this.auth = window.gapi.auth2.getAuthInstance();
-              this.setState({isSignedIn: this.auth.isSignedIn.get()})
+              this.setState({isSignedIn: this.auth.isSignedIn.get()});
+              this.auth.isSignedIn.listen(this.onAuthChange)
             });
         });
     }
 
+    onAuthChange = () =>{
+        this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+    }
+
+    onSignIn = () => {
+this.auth.signIn();
+    };
+
+    onSignOut = () => {
+ this.auth.signOut();
+    };
+
+
     renderAuthButton(){
         if(this.state.isSignedIn === null){
-            return <div>You probably not signed in.</div>
+            return null;
         }else if (this.state.isSignedIn){
-            return <div>You're signed in!</div>
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                      <i className="google icon" />
+                      Sign Out
+                 </button>   
+            )
         }else{
-            return <div>You're not signed in!</div>
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                <i className="google icon" />
+                Sign In with Google
+           </button>   
+            )
         }
     }
     render(){
