7fcfac734fb3dcdfdae01150e7f0f4ed9f90f567
Heneros <punk60826@gmail.com>
2021-11-11 23:56:46 +0200

    modal props

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -5,7 +5,7 @@ import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component{
   state = { isSignedIn: null};
-  
+    
     componentDidMount(){
         window.gapi.load('client:auth2', () =>{
             window.gapi.client.init({
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
 const Modal = props =>{
    return ReactDOM.createPortal(
-       <div className="ui dimmer modals visib;e active">
-        <div className="ui standard modal visible active">
-            helloooooooooo
-            gfdgdffg gdfg fd
+       <div onClick={props.onDismiss} className="ui dimmer modals visib;e active">
+        <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+        <div className="header">
+          {props.title}
+           </div>
+           <div className="content">
+          {props.content}
+           </div>
+           <div className="actions">
+           {props.actions}
+               </div>
         </div>
         </div>,
         document.querySelector('#modal')
